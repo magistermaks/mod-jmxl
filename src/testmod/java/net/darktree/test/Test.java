aeef65104c559316ead7409f4441006cfa7c3b23
magistermaks <magistermaks@darktree.net>
2022-06-17 12:43:12 +0200

    Backported to 1.18

--- src/testmod/java/net/darktree/test/Test.java
+++ src/testmod/java/net/darktree/test/Test.java
@@ -10,7 +10,7 @@ import net.minecraft.util.registry.Registry;
 
 public class Test implements ModInitializer {
 
-	public final static Block TEST_BLOCK = new Block(FabricBlockSettings.of(Material.REDSTONE_LAMP).strength(0.3F).sounds(BlockSoundGroup.GLASS));
+	public final static Block TEST_BLOCK = new Block(FabricBlockSettings.of(Material.REDSTONE_LAMP).nonOpaque().strength(0.3F).sounds(BlockSoundGroup.GLASS));
 
 	@Override
 	public void onInitialize() {
